6850a11821b3ba517d3ef18cb71d993152b5162f
James Gennoy <james@gennoy.com>
2014-04-10 15:34:46 -0700

    If more than one movie is returned try to match by year

--- js/netflix.js
+++ js/netflix.js
@@ -4,6 +4,14 @@ movieFinder.searchMovies(movieTitle.html(), function(data){
   movies = data;
   if (movies.movies.length === 1) {
     movieTitle.after(movieFinder.buildForNetflix(movies.movies[0]));
+  } else if (movies.movies.length > 1) {
+    var year = $('span.year').html();
+    for (var i = 0; i < movies.movies.length; i++) {
+      if (movies.movies[i].year == year) {
+        movieTitle.after(movieFinder.buildForNetflix(movies.movies[i]));
+        break;
+      }
+    }
   }
 });
 chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
