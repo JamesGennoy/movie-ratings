3be7ac8407d8e2fa84a53c773f3e1d4accd77961
James Gennoy <james@gennoy.com>
2014-04-11 15:18:37 -0700

    Changed to use the OMDb API

--- js/moviefinder.js
+++ js/moviefinder.js
@@ -1,13 +1,11 @@
 var movieFinder = (function() {
 
-    function _searchMovies(title, callback) {
-        var url = 'http://api.rottentomatoes.com/api/public/v1.0/movies.json?' +
-            'apikey=8yufrh8fdusxj9k5yqxqhafs&' +
-            'q=' + encodeURIComponent(title) + '&' +
-            'page_limit=10&' +
-            'page=1';
+    function _findMovieById(imdbID, callback) {
+        var url = 'http://www.omdbapi.com/?' +
+            'i=' + imdbID +
+            '&tomatoes=true';
         var xhr = new XMLHttpRequest();
-        xhr.open("GET", url, true);
+        xhr.open("GET", url, false);
         xhr.onreadystatechange = function() {
             if (xhr.readyState == 4) {
                 var resp;
@@ -20,38 +18,111 @@ var movieFinder = (function() {
                 callback(resp);
             }
         }
-        xhr.send();
+        xhr.send(null);
+    }
+
+    function _searchMovies(title, year, callback) {
+        var url = 'http://www.omdbapi.com/?' +
+            's=' + encodeURIComponent(title) +
+            '&y=' + year;
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", url, false);
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState == 4) {
+                var resp;
+                try {
+                    resp = JSON.parse(xhr.responseText);
+                } catch (e) {
+                    resp = '';
+                }
+                console.log(resp);
+                callback(resp);
+            }
+        }
+        xhr.send(null);
+    }
+    
+    function _findMovieByTitle(title, year, callback) {
+        var url = 'http://www.omdbapi.com/?' +
+            't=' + encodeURIComponent(title) +
+            '&y=' + year +
+            '&tomatoes=true';
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", url, false);
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState == 4) {
+                var resp;
+                try {
+                    resp = JSON.parse(xhr.responseText);
+                } catch (e) {
+                    resp = '';
+                }
+                console.log(resp);
+                callback(resp);
+            }
+        }
+        xhr.send(null);
     }
 
     return {
         buildForNetflix: function(movie) {
-            return '<p>Rotten Tomatoes: ' + movie.ratings.critics_score + '%</p>';
+            return '<p>Rotten Tomatoes: ' + movie.tomatoMeter + '%</p>' +
+                '<p>IMDB: ' + movie.imdbRating + '</p>' +
+                '<p>Metacritic: ' + movie.Metascore + '</p>';
         },
         buildForPopup: function(movies) {
             var contents = '';
             for (var i = 0; i < movies.length; i++) {
-                contents = contents.concat('<p>' + movies[i].title + ' (' + movies[i].year + ') - ' + movies[i].ratings.critics_score + '%</p>');
+                contents = contents.concat('<p>' + movies[i].Title + ' (' + movies[i].Year + ') - ' + movies[i].tomatoMeter + '% - ' + movies[i].imdbRating + ' - ' + movies[i].Metascore + '</p>');
             }
             return contents;
         },
         searchForNetflix: function(title, year, callback) {
             title = title.trim();
-            _searchMovies(title, function(data) {
-                var _movies = [];
-                for (var i = 0; i < data.movies.length; i++) {
-                    if (data.movies[i].year == year && data.movies[i].title == title) {
-                        _movies.splice(0, 0, data.movies[i]);
-                    } else {
-                        _movies.push(data.movies[i]);
-                    }
+            _findMovieByTitle(title, year, function(data) {
+                if (data && data.Response) {
+                    var _movies = [];
+                    _movies.push(data);
+                    callback(_movies);
+                } else {
+                    _searchMovies(title, year, function(data){
+                        var _movies = [];
+                        for (var i = 0; i < data.Search.length; i++) {
+                            _getMovieDetails(data.Search[i].imdbID, function(data){
+                                if (data.Year == year && data.Title == title) {
+                                    _movies.splice(0, 0, data);
+                                } else {
+                                    _movies.push(data);
+                                }
+                            });
+                        }
+                        callback(_movies);
+                    });
                 }
-                callback(_movies);
             });
         },
         searchForPopup: function(title, callback) {
             title = title.trim();
-            _searchMovies(title, function(data) {
-                callback(data.movies);
+            _findMovieByTitle(title, '', function(data) {
+                if (data && data.Response) {
+                    var _movies = [];
+                    _movies.push(data);
+                    callback(_movies);
+                } else {
+                    _searchMovies(title, '', function(data){
+                        var _movies = [];
+                        for (var i = 0; i < data.Search.length; i++) {
+                            _getMovieDetails(data.Search[i].imdbID, function(data){
+                                if (data.Year == year && data.Title == title) {
+                                    _movies.splice(0, 0, data);
+                                } else {
+                                    _movies.push(data);
+                                }
+                            });
+                        }
+                        callback(_movies);
+                    });
+                }
             });
         }        
     }
